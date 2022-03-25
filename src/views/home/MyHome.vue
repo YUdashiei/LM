dbe6d7c1eca5a5c529af34bab177ba6073393d30
YUdashiei <yudashiei@foxmail.com>
2022-03-25 22:36:14 +0800

    首页

---
+++ src/views/home/MyHome.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <Static-Part />
+    <My-Nearby />
+  </div>
+    <My-Docker />
+</template>
+
+<script>
+import MyDocker from './MyDocker.vue'
+import MyNearby from './MyNearby.vue'
+import StaticPart from './StaticPart.vue'
+export default {
+  components: { StaticPart, MyNearby, MyDocker },
+  name: 'MyHome'
+}
+</script>
+<style lang="scss" scoped>
+@import '../../style/viriables.scss';
+@import '../../style/mixins.scss';
+.wrapper {
+  overflow-y: auto;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: .5rem;
+  right: 0;
+  padding: 0 .18rem .2rem .18rem;
+}
+</style>
