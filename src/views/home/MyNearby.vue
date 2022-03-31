623c831c3985f2e9f67fb2680f044bec64f173b7
YUdashiei <yudashiei@foxmail.com>
2022-03-31 22:34:23 +0800

    首页

--- src/views/home/MyNearby.vue
+++ src/views/home/MyNearby.vue
@@ -1,42 +1,36 @@
 <template>
 <div class="nearby">
     <h3 class="nearby__title">附近店铺</h3>
-    <div class="nearby__item"
-         v-for="item in nearbyList"
-         :key="item.id"
-       >
-           <img :src="item.imgUrl" class="nearby__item__img">
-          <div class="nearby__content">
-            <div class="nearby__content__title">{{item.title}}</div>
-            <div class="nearby__content__tags">
-              <span class="nearby__content__tag"
-              v-for="(innerItem, innerIndex) in item.tags"
-              :key="innerIndex"
-              >{{innerItem}}</span>
-            </div>
-            <p class="nearby__content__highlight">{{item.desc}}</p>
-          </div>
-    </div>
+    <router-link
+      v-for="item in nearbyList"
+      :key="item._id"
+      :to="`/shop/${item._id}`">
+    <My-Shopinfo :item="item" />
+    </router-link>
 </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import { get } from '../../utils/request'
+import MyShopinfo from '../../components/MyShopinfo'
+
+const useNearbyListEffect = () => {
+  const nearbyList = ref([])
+  const getNearbyList = async () => {
+    const result = await get('/api/shop/hot-list')
+    if (result?.errno === 0 && result?.data?.length) {
+      nearbyList.value = result.data
+    }
+  }
+  return { nearbyList, getNearbyList }
+}
 export default {
   name: 'MyNearby',
+  components: { MyShopinfo },
   setup () {
-    const nearbyList = [{
-      id: 1,
-      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
-      title: '沃尔玛',
-      tags: ['月售1万+', '起送¥0', '基础运费¥5'],
-      desc: 'VIP尊享满89元减4元运费券（每月3张）'
-    }, {
-      id: 2,
-      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
-      title: '沃尔玛',
-      tags: ['月售1万+', '起送¥5', '基础运费¥5'],
-      desc: 'VIP尊享满89元减4元运费券（每月3张）'
-    }]
+    const { nearbyList, getNearbyList } = useNearbyListEffect()
+    getNearbyList()
     return { nearbyList }
   }
 }
@@ -51,39 +45,8 @@ export default {
     font-weight: normal;
     color: $content-fontcolor;
   }
-  &__item {
-    display: flex;
-    padding-top: .12rem;
-    &__img {
-      margin-right: .16rem;
-      width: .56rem;
-      height: .56rem;
-    }
-  }
-  &__content {
-    flex: 1;
-    padding-bottom: .12rem;
-    border-bottom: 1px solid $content-bgColor;
-    &__title {
-      line-height: .22rem;
-      font-size: .16rem;
-      color: $content-fontcolor;
-    }
-    &__tags {
-      margin-top: .08rem;
-      line-height: .18rem;
-      font-size: .13rem;
-      color: $content-fontcolor;
-    }
-    &__tag {
-      margin-right: .16rem;
-    }
-    &__highlight {
-      margin: .08rem 0 0 0;
-      line-height: .18rem;
-      font-size: .13rem;
-      color: #E93B3B;
-    }
+   a {
+    text-decoration: none;
   }
 }
 </style>
